36fbaada68c63d3006d614b9338181a1ef5864db
jihye <zehye.01@gmail.com>
2021-06-29 11:51:40 +0900

    web api manager 생성

--- solution/DFriends/DFriends/Managers/WebApiManager.swift
+++ solution/DFriends/DFriends/Managers/WebApiManager.swift
@@ -5,4 +5,32 @@
 //  Created by zehye on 2021/06/29.
 //
 
-import Foundation
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class WebApiManager: NSObject {
+    static let shared = WebApiManager()
+    let mainUrl = "https://docfriends.github.io/Docfriends_iOS_Recruit/api/home.json"
+    
+    override init() {
+        super.init()
+    }
+    
+    func projectList(success:@escaping (JSON) -> Void, failure:@escaping (Error) -> Void) {
+        let url = "\(mainUrl)"
+        AF.request(url, method: .get).responseJSON {(response) in
+            switch response.result {
+            case .success(_):
+                let json = JSON(response.value!)
+                success(json)
+                
+                break
+            case.failure(_):
+                let error: Error = response.error!
+                failure(error)
+                break
+            }
+        }
+    }
+}
